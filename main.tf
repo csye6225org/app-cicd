e6c7fb65b6eb4c08b1fc2a8e8c85e6c27c9bf703
Varad Lata Ratnakar Desai <varad.desai.rl@gmail.com>
2021-11-04 15:56:28 -0400

    Add policies

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
+// ############################################
+// # GH-Upload-To-S3
+// ############################################
 
-resource "aws_iam_policy" "GH-Upload-To-S3" {
+resource "aws_iam_policy" "policy_1" {
   name        = "GH-Upload-To-S3"
   description = "Github actions application policy"
   policy      = <<EOF
 {
   "Version": "2012-10-17",
-  "Statement": [
+  "Statement": 
+  [
     {
       "Action": [
         "s3:PutObject",
@@ -23,7 +27,60 @@ resource "aws_iam_policy" "GH-Upload-To-S3" {
 EOF
 }
 
-resource "aws_iam_user_policy_attachment" "ghaction_app_policy_attachment" {
+resource "aws_iam_user_policy_attachment" "policy_attachment_1" {
   user       = var.aws_iam_user
-  policy_arn = aws_iam_policy.GH-Upload-To-S3.arn
+  policy_arn = aws_iam_policy.policy_1.arn
+}
+
+// ############################################
+// # GH-Code-Deploy
+// ############################################
+
+resource "aws_iam_policy" "policy_2" {
+  name        = "GH-Code-Deploy"
+  description = "Github actions codedeploy policy"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": 
+  [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codedeploy:RegisterApplicationRevision",
+        "codedeploy:GetApplicationRevision"
+      ],
+      "Resource": [
+        "arn:aws:codedeploy:${var.aws_region}:${var.aws_account_id}:application:${var.aws_codedeploy_application_name}"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codedeploy:CreateDeployment",
+        "codedeploy:GetDeployment"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codedeploy:GetDeploymentConfig"
+      ],
+      "Resource": [
+        "arn:aws:codedeploy:${var.aws_region}:${var.aws_account_id}:deploymentconfig:CodeDeployDefault.OneAtATime",
+        "arn:aws:codedeploy:${var.aws_region}:${var.aws_account_id}:deploymentconfig:CodeDeployDefault.HalfAtATime",
+        "arn:aws:codedeploy:${var.aws_region}:${var.aws_account_id}:deploymentconfig:CodeDeployDefault.AllAtOnce"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "policy_attachment_2" {
+  user       = var.aws_iam_user
+  policy_arn = aws_iam_policy.policy_2.arn
 }
--- variables.tf
+++ variables.tf
@@ -6,3 +6,15 @@ variable "aws_codedeploy_bucket" {
   type        = string
   description = "AWS S3 Bucket for code deploy"
 }
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+}
+variable "aws_account_id" {
+  type        = string
+  description = "AWS account id"
+}
+variable "aws_codedeploy_application_name" {
+  type        = string
+  description = "AWS codedeploy application name"
+}
